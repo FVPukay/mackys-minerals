d5d31457fa9af8b6bcc4e6dc6d9e06b1d31a1d96
frederickpukay <frederickpukay@gmail.com>
2019-08-19 14:54:37 -0700

    feat: Add MineralData model

--- minerals_app/models.py
+++ minerals_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.db.models.functions import Lower
+from django.urls import reverse
 
-# Create your models here.
+
+class MineralData(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    image_filename = models.CharField(max_length=255, unique=True)
+    image_caption = models.CharField(max_length=500)
+    category = models.CharField(max_length=500)
+    group = models.CharField(max_length=500)
+
+    formula = models.CharField(max_length=500, default='', blank=True)
+    strunz_classification = models.CharField(max_length=500, default='', blank=True)
+    crystal_system = models.CharField(max_length=500, default='', blank=True)
+    unit_cell = models.CharField(max_length=500, default='', blank=True)
+    color = models.CharField(max_length=500, default='', blank=True)
+    crystal_symmetry = models.CharField(max_length=500, default='', blank=True)
+    cleavage = models.CharField(max_length=500, default='', blank=True)
+    mohs_scale_hardness = models.CharField(max_length=500, default='', blank=True)
+    luster = models.CharField(max_length=500, default='', blank=True)
+    streak = models.CharField(max_length=500, default='', blank=True)
+    diaphaneity = models.CharField(max_length=500, default='', blank=True)
+    optical_properties = models.CharField(max_length=500, default='', blank=True)
+    refractive_index = models.CharField(max_length=500, default='', blank=True)
+    crystal_habit = models.CharField(max_length=500, default='', blank=True)
+    specific_gravity = models.CharField(max_length=500, default='', blank=True)
+
+    class Meta:
+        indexes = [models.Index(fields=['name'])]
+        ordering = [Lower("name")]
+
+    def __str__(self):
+        return f"{self.name}"
