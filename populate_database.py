6bb0eb0e93366245261dae31a91c4343274143eb
frederickpukay <frederickpukay@gmail.com>
2019-08-19 15:13:34 -0700

    feat: Add database population script

---
+++ populate_database.py
@@ -0,0 +1,20 @@
+import json
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mineral_catalog.settings')
+
+import django
+django.setup()
+
+from minerals_app.models import MineralData
+
+def populate():
+    with open('minerals.json') as file:
+        file = json.loads(file.read())
+
+    for mineral in file:
+        mineral_entry = MineralData.objects.get_or_create(**mineral)
+
+if __name__ == '__main__':
+    print('Populating database')
+    populate()
+    print('Database populating complete')
