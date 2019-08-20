894e947c916e1e2529777ec6fb70a4e452673c97
frederickpukay <frederickpukay@gmail.com>
2019-08-20 15:40:30 -0700

    refactor: Change urls organization

--- mineral_catalog/urls.py
+++ mineral_catalog/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from minerals_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('minerals_app.urls')),
+    path('', views.MineralDataListView.as_view(), name='minerals_list'),
 ]
