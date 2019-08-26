60fad0b8c36c5cd0bf7ff413fb3e3809a1867556
frederickpukay <frederickpukay@gmail.com>
2019-08-26 10:08:23 -0700

    refactor: Add context object name for ListView

--- minerals_app/views.py
+++ minerals_app/views.py
@@ -6,6 +6,7 @@ from minerals_app.models import Mineral
 class MineralListView(ListView):
     model = Mineral
     template_name = 'minerals_app/index.html'
+    context_object_name = 'minerals'
 
     def get_queryset(self):
         return Mineral.objects.all()
