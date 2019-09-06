59274186247fc988dee733901212722e772a9c83
frederickpukay <frederickpukay@gmail.com>
2019-09-05 18:40:28 -0700

    fix: Edited STATICFILES_STORAGE due to compatibility issue with WhiteNoise v4.0

--- mineral_catalog/deploy_settings/__init__.py
+++ mineral_catalog/deploy_settings/__init__.py
@@ -15,4 +15,4 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
