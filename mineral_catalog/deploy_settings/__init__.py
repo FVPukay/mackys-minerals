fbf0898ef5e2a57ddd8b83845f9686321e8c1b22
frederickpukay <frederickpukay@gmail.com>
2019-09-05 18:34:17 -0700

    chore: Add whitenoise to project

--- mineral_catalog/deploy_settings/__init__.py
+++ mineral_catalog/deploy_settings/__init__.py
@@ -14,3 +14,5 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
