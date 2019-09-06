c84d872c68c830df7d0366a67af5fa9cc6e0cddd
frederickpukay <frederickpukay@gmail.com>
2019-09-05 19:00:29 -0700

    fix: Swap WhiteNoise storage backend for the Django one

--- mineral_catalog/settings.py
+++ mineral_catalog/settings.py
@@ -25,7 +25,7 @@ def get_env_variable(var_name):
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
 # Quick-start development settings - unsuitable for production
